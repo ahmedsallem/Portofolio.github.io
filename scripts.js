2a6cb6662e5222282c7a5a3b1ece2428a24e5355
ahmed_salem <100295040+ahmedsallem@users.noreply.github.com>
2024-12-16 15:51:50 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Example of a simple script for interactivity (optional)
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Welcome to Ahmed Ibrahim Salem's portfolio!");
+});
